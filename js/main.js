110f977e52f1e78c1341ae542c7f50ff6c3cce2c
Shoaibvanu <156096634+Shoaibvanu@users.noreply.github.com>
2024-02-12 16:12:56 +0530

    sub commit

--- js/main.js
+++ js/main.js
@@ -477,33 +477,15 @@ if ($(".curved-circle").length) {
 const popupOverlay = document.querySelector(".popup-overlay");
 const skipButton = document.querySelector(".popup-container .skip-button");
 
-let remainingTime = 5;
-let allowedToSkip = false;
-let popupTimer;
-
 const showAd = () => {
   popupOverlay.classList.add("active");
-  popupTimer = setInterval(() => {
-    skipButton.innerHTML = `Skip in ${remainingTime}s`;
-    remainingTime--;
-
-    if (remainingTime < 0) {
-      allowedToSkip = true; // Update allowedToSkip to true when the timer is done
-      skipButton.innerHTML = "Skip";
-      clearInterval(popupTimer);
-    }
-  }, 1000);
 };
 
 const skipAd = () => {
   popupOverlay.classList.remove("active");
 };
 
-skipButton.addEventListener("click", () => {
-  if (allowedToSkip) {
-    skipAd();
-  }
-});
+skipButton.addEventListener("click", skipAd);
 
 const startTimer = () => {
   if (window.scrollY > 100) {
@@ -513,3 +495,4 @@ const startTimer = () => {
 };
 
 window.addEventListener("scroll", startTimer);
+
